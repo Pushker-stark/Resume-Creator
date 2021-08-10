a36a89a6c7dfca5e0dac8fc7f8bded0c2e1f63e0
P. Naresh <64632590+Pushker-stark@users.noreply.github.com>
2021-08-10 14:19:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
-    include_once("res.html");   
+
+    include_once("res.html"); 
+    function function_alert($message) {
+      
+        // Display the alert box 
+        echo "<script>alert('$message');</script>";
+    }
+      
+      
+    // Function call
+    function_alert("Welcome to Quollab resume builder kindly fill  all details and upload image before submitting!!!");  
 
 ?>
